188660da35bd0a77c7eef723e8e8d545643dcb53
nathanealjr19 <79429416+nathanealjr19@users.noreply.github.com>
2022-07-20 12:26:19 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBpHqS8DOxLm2DdwpmJ2VXzJ5yGkKzS1c4",
+    authDomain: "kwitter-project-fc9db.firebaseapp.com",
+    databaseURL: "https://kwitter-project-fc9db-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-fc9db",
+    storageBucket: "kwitter-project-fc9db.appspot.com",
+    messagingSenderId: "243641874375",
+    appId: "1:243641874375:web:69d158e2d1bee7e5d94be2"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function addRoom(){
+
+}
+function Logout(){
+    
+}
